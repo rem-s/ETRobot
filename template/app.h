a95ca0f2800277e6a696a4833e4f4f1f067b43f0
HayatoKato <aaaaaaaaainjgvd@gmail.com>
2019-05-26 04:24:03 +0900

    ADD CYCLE_TEMP

---
+++ template/app.h
@@ -0,0 +1,42 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ev3api.h"
+
+/*
+ *  Queueの最大サイズをマクロで宣言
+ */
+#define MAX_QUEUE 11
+
+/*
+ *  各タスクの優先度の定義
+ */
+#define MAIN_PRIORITY    TMIN_APP_TPRI + 1  /* メインタスクの優先度 */
+#define SUB_PRIORITY    TMIN_APP_TPRI + 2  /* メインタスクの優先度 */
+/*
+ *  ターゲットに依存する可能性のある定数の定義
+ */
+#ifndef STACK_SIZE
+#define STACK_SIZE      4096        /* タスクのスタックサイズ */
+#endif /* STACK_SIZE */
+
+/*
+ *  関数のプロトタイプ宣言
+ */
+#ifndef TOPPERS_MACRO_ONLY
+
+extern void main_task(intptr_t exinf);
+void initialize();
+void create_system();
+void delete_system();
+void led_change();
+void state_change();
+extern void sub_task();
+extern void timer_act_1sec(intptr_t exinf);
+
+#endif /* TOPPERS_MACRO_ONLY */
+
+#ifdef __cplusplus
+}
+#endif
