e18844f7a2643f4b6796be14c6f6903d3d3310c0
ReonNamatame <reon64.1173@gmail.com>
2019-07-20 17:59:27 +0900

    init

--- Program/line_trace.c
+++ Program/line_trace.c
@@ -3,5 +3,30 @@
 //カラー値よりTurnの値を決定する
 void line_trace(float color_value, int *turn)
 {
-	*turn = 0;
+	//ON/OFF制御
+/*
+	if(color_value < BLACK) *turn = 20;
+	else *turn = -20;
+*/
+	//変数の宣言
+	static float p, i, d = 0;
+	static float e = 0, e_last = 0;
+	static float integral = 0;
+	static float differential = 0;
+
+	//偏差eを更新
+	e_last = e;
+	//偏差e ＝（目標値）ー（現在時間ｔの取得値）
+	e = BLACK - color_value;
+	//積分値
+	integral += e / 2.0 * DELTA_T;
+	//微分値
+	differential = (e - e_last) / DELTA_T;
+	
+	p = KP * e;
+	i = KI * integral;
+	d = KD * differential;
+
+	*turn = p + i + d;
+	//*turn = 0;
 }
--- Program/line_trace.h
+++ Program/line_trace.h
@@ -7,6 +7,11 @@ extern "C" {
 //マクロ
 #define BLACK 20
 
+#define KP 0.2
+#define KI 0.4
+#define KD 0.4
+#define DELTA_T 0.004
+
 //通常の関数
 void line_trace(float color_value, int *turn);
 
