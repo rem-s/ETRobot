4a66717cb4d303d6626bccf4c90295f00e758c93
Aki49 <Akawabe049@gmail.com>
2019-09-20 02:28:28 +0900

    add Program6 directory

---
+++ Program6/lookup_line_trace.h
@@ -0,0 +1,41 @@
+#ifndef __LOOKUP_LINE_TRACE_HEADER__
+#define __LOOKUP_LINE_TRACE_HEADER__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef TOPPERS_MACRO_ONLY
+
+//マクロ
+// #define DESIRED_VALUE2 13
+
+// #define KU2 0.650
+#define KU2 0.700
+#define PU2 0.60
+#define TI2 0.50*PU2
+#define TD2 0.1250*PU2
+// #define A2 1.0
+
+// #define KP2 0.60f*KU2
+// #define KP2 0.800
+#define KP2 0.800
+#define KI2 KP2/TI2
+#define KD2 KP2*TD2
+#define DELTA_T2 0.0040
+
+//通常の関数
+void lookup_line_trace(float color_value, int *turn);
+
+extern float e2;
+extern float p2;
+extern float i2;
+extern float d2;
+
+#endif /* TOPPERS_MACRO_ONLY */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Program6/sensor_process.c
@@ -0,0 +1,33 @@
+#include "sensor_process.h"
+
+// FIRフィルタ係数(カットオフ周波数45Hz)
+static float hw[N] = {-0.00299, -0.01312, -0.0105, 0.08365, 0.26271, 0.36000, 0.26271, 0.08365, -0.0105, -0.01312, -0.00299};
+
+// 11個のカラーの値を補完するための配列
+static float color_N[N];
+
+//フィルタリング
+void filtering(int sensor_value, float *filtered_value)
+{
+	int i;
+    for(i=0; i < N-1; i++) color_N[N-1-i] = color_N[N-2-i];
+    color_N[0] = (float)sensor_value;
+	
+	//センサ出力初期化
+	*filtered_value = 0.0;
+	
+	//最初センサ値を初期値に統一する
+	if(first_color_flag==0) for(i=0; i < N; i++) color_N[i] = sensor_value;
+	first_color_flag = 1;
+	
+	// FIRフィルタ計算
+    for(i=0; i < N; i++) {
+        *filtered_value += color_N[i] * hw[i];
+    }
+}
+
+//センサー正規化
+void normalization(float sensor_value, float *normalized_value)
+{
+	*normalized_value = sensor_value;
+}
